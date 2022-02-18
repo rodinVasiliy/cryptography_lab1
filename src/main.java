ebff15716fbed7e1940349f4a9adbed9b76bd2f5
viuiuuita <kudracsova@mail.ru>
2022-02-18 12:17:59 +0400

    added console interface

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
 
@@ -24,7 +25,21 @@ public class main {
     }
 
     public static void main(String[] args) {
-        String path = "C:\\IDEA_Projects\\cryptography_lab1\\doyle_the_adventures.txt";
+        System.out.println("Select mode:");
+        System.out.println("1) encrypt");
+        System.out.println("2) decrypt");
+        System.out.println("0) exit");
+
+        Scanner console = new Scanner(System.in);
+        int option = console.nextInt();
+        if (option == 0) return;
+        if (option == 1){
+            System.out.print("Enter the path of the text: ");
+            String path = console.nextLine();
+            System.out.print("Enter the path of the key: ");
+            String key = console.nextLine();
+        }
+        /*String path = "C:\\Users\\HUAWEI\\IdeaProjects\\cryptography_lab1\\doyle_the_adventures.txt";
         String text = getText(path);
         if (text == null) {
             System.out.println("text not found");
@@ -40,7 +55,7 @@ public class main {
 
         Decoder decoder = new Decoder(Constants.ALPH, encodedText);
         String decodedText = decoder.decodeText();
-        System.out.println(decodedText);
+        System.out.println(decodedText);*/
 
     }
 
